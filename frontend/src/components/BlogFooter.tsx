4dab713ff9697dddc168c336c343ebf277c8d35d
Mayank <mayanktiwari1212.mt@gmail.com>
2024-07-29 17:25:01 +0530

    footer updated

--- frontend/src/components/BlogFooter.tsx
+++ frontend/src/components/BlogFooter.tsx
@@ -1,8 +1,8 @@
 const BlogFooter = () => {
   return (
-    <div className="font-myFont py-4 px-6 w-full flex justify-center items-center bg-backgroundDark absolute bottom-0">
+    <footer className="font-myFont py-4 px-6 w-full flex justify-center items-center bg-backgroundDark">
       <span>&copy; 2024 mlogs. All rights reserved.</span>
-    </div>
+    </footer>
   );
 };
 
