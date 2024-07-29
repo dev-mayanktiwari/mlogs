2ddc329bebb012b7dd56906d7ae2de8335028970
Mayank <mayanktiwari1212.mt@gmail.com>
2024-07-29 18:49:59 +0530

    env

--- frontend/src/backendUrl.ts
+++ frontend/src/backendUrl.ts
@@ -1,3 +1,7 @@
-const BACKEND_URL = "https://backend.mayankiwari-mt.workers.dev";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export default BACKEND_URL;
