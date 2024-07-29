2c2b333d5d58a80face3010fb430a0ac913415c5
Mayank <mayanktiwari1212.mt@gmail.com>
2024-07-29 18:12:14 +0530

    puri file badal di

--- frontend/src/pages/UnitBlogPage.tsx
+++ frontend/src/pages/UnitBlogPage.tsx
@@ -2,39 +2,42 @@ import { useParams } from "react-router-dom";
 import useUnitBlog from "../hooks/useUnitBlog";
 import Navbar from "../components/Navbar";
 import formatDate from "../utils/dateExtractor";
-// import unitBlogSkeleton from "../utils/untiBlogSkeleton";
 import BlogFooter from "../components/BlogFooter";
-// import blogSkeleton from "../utils/blogSkeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const UnitBlogPage = () => {
   const { id } = useParams<{ id: string }>();
-  // console.log(id)
   const { blog, loading, error } = useUnitBlog(id!);
-
-  //   if (loading) {
-  //     return unitBlogSkeleton();
-  //   }
-
-  //   if (error) {
-  //     return <p className="text-red-500">{error}</p>;
-  //   }
-
-  if (!blog) {
-    return <p>No blog found.</p>
-  }
 
   return (
     <>
       <Navbar />
-      {loading && <p className="text-center">Loading...</p>} 
-      {error && <p className="text-red-500 text-center">{error}</p>}
-      {!blog && <p className="text-red-500 text-center">No blog found.</p>}
       <div className="bg-backgroundDark font-myFont py-8 px-12 mt-16 min-h-screen text-primary-foreground">
-        <h1 className="text-3xl font-bold pb-2">{blog?.title}</h1>
-        <p>{formatDate(blog.createdAt)}</p>
-        <p className="pt-5">{blog?.content}</p>
+        {loading ? (
+          <SkeletonTheme>
+            <h1 className="text-3xl font-bold pb-2">
+              <Skeleton height={40} />
+            </h1>
+            <p>
+              <Skeleton width={150} />
+            </p>
+            <div className="pt-5">
+              <Skeleton count={5} />
+            </div>
+          </SkeletonTheme>
+        ) : error ? (
+          <p className="text-red-500 text-center">{error}</p>
+        ) : blog ? (
+          <>
+            <h1 className="text-3xl font-bold pb-2">{blog.title}</h1>
+            <p>{formatDate(blog.createdAt)}</p>
+            <p className="pt-5">{blog.content}</p>
+          </>
+        ) : (
+          <p className="text-red-500 text-center">No blog found.</p>
+        )}
       </div>
-
       <BlogFooter />
     </>
   );
